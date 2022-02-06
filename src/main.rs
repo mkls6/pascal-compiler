cc6e27937fff5312ed7a03c33c859f060c607810
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2022-02-06 21:12:49 +0500

    Add struct to represent tokens

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod token;
+
 fn main() {
-    println!("Hello, world!");
+    let my_integer = token::Integer::new(5);
+    println!("My integer {}", my_integer);
+
+    let my_real = token::Real::new(3.25);
+    println!("My real {}", my_real);
+
+    let another_real = token::Real::from_string(String::from("44.25")).unwrap();
+    println!("Another real {}", another_real);
+    println!("String representation: {}", another_real.as_string());
+
+    let my_boolean = token::Boolean::from_string(String::from("true")).unwrap();
+    println!("My boolean {}", my_boolean);
+
+    let my_string = token::Str::from_string(String::from("MyString")).unwrap();
+    println!("My string {}", my_string);
+
+    println!("Actual value: {}", my_boolean.as_value())
 }
