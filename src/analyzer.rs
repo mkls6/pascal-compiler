2519e10c2786be1bc9a681aab05e0abae1d2beba
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2022-06-12 15:17:52 +0500

    Reformat

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,15 +1,15 @@
 use crate::error::CompilerError;
-use crate::syntax::*;
 use crate::scope::{Scope, Usage};
+use crate::syntax::*;
 
 pub struct Analyzer {
-    scopes: Vec<Scope>
+    scopes: Vec<Scope>,
 }
 
 impl Analyzer {
     pub fn new() -> Self {
         Self {
-            scopes: Vec::from([Scope::default()])
+            scopes: Vec::from([Scope::default()]),
         }
     }
 
@@ -22,13 +22,19 @@ impl Analyzer {
     }
 
     /// Check if identifier is already defined
-    pub fn check_declaration(&mut self, decl: VarDeclaration) -> Result<VarDeclaration, CompilerError> {
+    pub fn check_declaration(
+        &mut self,
+        decl: VarDeclaration,
+    ) -> Result<VarDeclaration, CompilerError> {
         let len = self.scopes.len();
         let cur_scope = &mut self.scopes[len - 1];
         let str = decl.id.get_id();
 
         match cur_scope.get(str.clone()) {
-            Some(_) => Err(CompilerError::semantic(format!("Redeclaration of {:?}", str), decl.id.id.pos)),
+            Some(_) => Err(CompilerError::semantic(
+                format!("Redeclaration of {:?}", str),
+                decl.id.id.pos,
+            )),
             None => {
                 cur_scope.insert(&decl.id, Usage::Variable);
                 Ok(decl)
@@ -43,21 +49,22 @@ impl Analyzer {
             let cur_scope = scopes.next();
 
             if cur_scope.is_none() {
-                break Err(CompilerError::semantic(format!("Unknown identifier {:?}", id.get_id()), id.id.pos))
+                break Err(CompilerError::semantic(
+                    format!("Unknown identifier {:?}", id.get_id()),
+                    id.id.pos,
+                ));
             } else {
                 match cur_scope.unwrap().get(id.get_id()) {
                     Some(u) if u == usg => break Ok(()),
-                    Some(u) => break Err(CompilerError::semantic(format!("Identifier<{:?}> found, expected <{:?}>", u, usg), id.id.pos)),
-                    None => continue
+                    Some(u) => {
+                        break Err(CompilerError::semantic(
+                            format!("Identifier<{:?}> found, expected <{:?}>", u, usg),
+                            id.id.pos,
+                        ))
+                    }
+                    None => continue,
                 }
             }
         }
-    }
-
-    pub fn check_identifier(&self, id: &Identifier) {
-        todo!()
-    }
-    pub fn check_identifier_type(&self, id: &Identifier) {
-        todo!()
     }
 }
